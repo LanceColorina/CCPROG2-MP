872b83b71a922794037f49096bb6defef1d47cee
Lance Colorina <125275904+LanceColorina@users.noreply.github.com>
2023-03-20 01:00:25 +0800

    Update ColorinaLanceC.c
    fixed multiple implicit declaration warnings

--- ColorinaLanceC.c
+++ ColorinaLanceC.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 #include<conio.h>
+#include<stdlib.h>
+
+
 typedef struct records{
 	char topic[21];
 	char question[151];
@@ -9,6 +12,88 @@ typedef struct records{
 	char choice3[31];
 	char answer[31];
 } records;
+
+
+int passWordCheck(char password[], int n, char EnteredPassword[])
+{
+	int i;
+	for(i = 0; i < n;i++)
+	{
+		if(EnteredPassword[i] != password[i])
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void ManageStart(char password[], int n, char *result, records quiz[])
+{
+	int i,pick;
+	char choice;
+	char EnteredPassword[n];
+	while (passWordCheck(password, n, EnteredPassword) != 0)
+	{
+		printf("Please Enter your password: ");
+		
+		for(i = 0; i < n; i++)
+		{
+			EnteredPassword[i] = getch();
+			printf("*");
+		}
+			EnteredPassword[i] = '\0';
+			printf("\n");
+	
+		passWordCheck(password, n, EnteredPassword);
+		if (passWordCheck(password, n, EnteredPassword) == 1)
+		{
+			printf("Incorrect Password, would you like to try again or go back to the main menu?(Y - try again, N - go back)\n");
+			scanf("%c", &choice);
+			scanf("%c", &choice);
+			
+			
+			switch(choice)
+			{
+				case 'Y':
+					passWordCheck(password, n, EnteredPassword);
+					break;
+				case 'N':
+					*result = '\0';
+					interface(&*result, password, quiz);
+					break;
+				default:
+					do{
+						pick = 1;
+						printf("Character Input is not listed in the Directory please try again\n");
+						printf("Incorrect Password, would you like to try again or go back to the main menu?(Y - try again, N - go back)\n");
+						scanf("%c", &choice);
+						scanf("%c", &choice);
+						if(choice == 'Y'){
+							pick = 0;
+							ManageStart(password, strlen(password), &*result, quiz);
+						}
+						if (choice == 'N')
+						{
+							pick = 0;
+							*result = '\0';
+							interface(result, password, quiz);
+						}
+					}while(pick != 0);
+					
+					break;
+			}
+		
+		}
+	}
+	
+}
+
+void Exit()
+{
+	printf("You are now exiting the program\n");
+	printf("Thank you for playing");
+}
+
 
 
 void addRecord(){
@@ -22,82 +107,9 @@ void editRecord(){
 void deleteRecord(){
 	printf("You are now deleting a record");
 }
-int password(char password[], int n, char EnteredPassword[])
-{
-	int i;
-	for(i = 0; i < n;i++)
-	{
-		if(EnteredPassword[i] != password[i])
-		{
-			return 1;
-		}
-	}
-	return 0;
-}
 
-void ManageStart(char passWord[], int n, char *result)
-{
-	int i,pick;
-	char choice;
-	char EnteredPassword[n];
-	while (password(passWord, n, EnteredPassword) != 0)
-	{
-		printf("Please Enter your password: ");
-		
-		for(i = 0; i < n; i++)
-		{
-			EnteredPassword[i] = getch();
-			printf("*");
-		}
-			EnteredPassword[i] = '\0';
-			printf("\n");
-	
-		password(passWord, n, EnteredPassword);
-		if (password(passWord, n, EnteredPassword) == 1)
-		{
-			printf("Incorrect Password, would you like to try again or go back to the main menu?(Y - try again, N - go back)\n");
-			scanf("%c", &choice);
-			scanf("%c", &choice);
-			
-			
-			switch(choice)
-			{
-				case 'Y':
-					password(passWord, n, EnteredPassword);
-					break;
-				case 'N':
-					*result = '\0';
-					interface(result, passWord);
-					break;
-				default:
-					do{
-						pick = 1;
-						printf("Character Input is not listed in the Directory please try again\n");
-						printf("Incorrect Password, would you like to try again or go back to the main menu?(Y - try again, N - go back)\n");
-						scanf("%c", &choice);
-						scanf("%c", &choice);
-						if(choice == 'Y'){
-							pick = 0;
-							password(passWord, n, EnteredPassword);
-						}
-						if (choice == 'N')
-						{
-							pick = 0;
-							*result = '\0';
-							interface(result, passWord);
-						}
-					}while(pick != 0);
-					
-					break;
-			}
-		
-		}
-	}
-	
-}
 
 void recordManager(char *result, char password[], records quiz[]){
-	system("cls");
 	int choice1;
 	char choice2;
 	printf("Welcome to the manager data interface, which features would you like to use? (1 - Add, 2 - Edit, 3 - Delete) ");
@@ -124,44 +136,13 @@ void recordManager(char *result, char password[], records quiz[]){
 			scanf("%c", &choice2);
 			if(choice2 == '2'){
 				*result = '\0';
-				interface(&*result, password);
+				interface(&*result, password, quiz);
 			}
 			if(choice2 == '1'){
 				recordManager(&*result, password, quiz);
 			}
 			
 			break;
-	}
-}
-
-void interface(char *result, char password[], records quiz[])
-{
-
-	while(*result == '\0')
-	{
-		printf("Welcome to who doesn't want to fail CCPROG2!!!\nwhich interface would you like to use? (1 - Manage Data, 2 - Play, 3 - Exit) ");
-		scanf("%c", &*result);
-			switch(*result)
-			{
-				case '1':
-					system("cls");
-					ManageStart(password, strlen(password), &*result);
-					recordManager(&*result, password, quiz);
-				 	break;
-				case '2':
-					system("cls");
-					PlayInterface(&*result, password,quiz);
-					break;
-				case '3':
-					system("cls");
-					Exit();
-					break;
-				default:
-					printf("The Character is not listed in the directory, Please enter a new Character.\n");
-					*result = '\0';
-					system("cls");
-					break;	
-			}
 	}
 }
 
@@ -196,12 +177,6 @@ void PlayInterface(char *result, char password[], records quiz[])
 	
 }
 
-void Exit()
-{
-	printf("You are now exiting the program\n");
-	printf("Thank you for playing");
-	return 0;
-}
 
 int main()
 {
@@ -209,8 +184,38 @@ int main()
 	char password[21] = "Lance";
 	records quiz[7];
 	
-	interface(&result, password,quiz);
+	interface(&result, password, quiz);
 	
 	return 0;
 	
+}
+void interface(char *result, char password[], records quiz[])
+{
+
+	while(*result == '\0')
+	{
+		printf("Welcome to who doesn't want to fail CCPROG2!!!\nwhich interface would you like to use? (1 - Manage Data, 2 - Play, 3 - Exit) ");
+		scanf("%c", &*result);
+			switch(*result)
+			{
+				case '1':
+					system("cls");
+					ManageStart(password, strlen(password), &*result, quiz);
+					recordManager(&*result, password, quiz);
+				 	break;
+				case '2':
+					system("cls");
+					PlayInterface(&*result, password,quiz);
+					break;
+				case '3':
+					system("cls");
+					Exit();
+					break;
+				default:
+					printf("The Character is not listed in the directory, Please enter a new Character.\n");
+					*result = '\0';
+					system("cls");
+					break;	
+			}
+	}
 }
